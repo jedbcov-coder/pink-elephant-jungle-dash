4e94ea2fe4bec43f6197fb422e875b636d859936
jedbcov-coder <252367415+jedbcov-coder@users.noreply.github.com>
2026-05-26 11:33:33 +0200

    Add standalone chunk type constants module

---
+++ src/game/chunks/chunkTypes.js
@@ -0,0 +1,27 @@
+// @ts-check
+
+export const CHUNK_TYPE_TEACHING = 'teaching';
+export const CHUNK_TYPE_FRUIT_GUIDE = 'fruit-guide';
+export const CHUNK_TYPE_JUMP_LOG = 'jump-log';
+export const CHUNK_TYPE_SLIDE_VINES = 'slide-vines';
+export const CHUNK_TYPE_SMASH_BLOCKS = 'smash-blocks';
+export const CHUNK_TYPE_RIVER_CROCS = 'river-crocs';
+export const CHUNK_TYPE_ENEMY_PATROL = 'enemy-patrol';
+export const CHUNK_TYPE_REWARD = 'reward';
+export const CHUNK_TYPE_BREATHER = 'breather';
+export const CHUNK_TYPE_COMBO = 'combo';
+export const CHUNK_TYPE_GATE = 'gate';
+
+export const ALL_CHUNK_TYPES = Object.freeze([
+  CHUNK_TYPE_TEACHING,
+  CHUNK_TYPE_FRUIT_GUIDE,
+  CHUNK_TYPE_JUMP_LOG,
+  CHUNK_TYPE_SLIDE_VINES,
+  CHUNK_TYPE_SMASH_BLOCKS,
+  CHUNK_TYPE_RIVER_CROCS,
+  CHUNK_TYPE_ENEMY_PATROL,
+  CHUNK_TYPE_REWARD,
+  CHUNK_TYPE_BREATHER,
+  CHUNK_TYPE_COMBO,
+  CHUNK_TYPE_GATE,
+]);
